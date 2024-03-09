302ca86b0c418b3cd2624f14320d280048fc7f56
Harry Morgan <psyhm6@nottingham.ac.uk>
2024-03-09 14:49:59 +0000

    feat: fetch pokemon data from api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, world!")
+interface Pokemon {
+  name: string;
+}
+
+async function getPokemon(): Promise<Pokemon[]> {
+  const response = await fetch ("https://pokeapi.co/api/v2/pokemon/?limit=20/") // fetch data from pokeapi website
+  const data = await response.json(); // convert the data to a json file
+  if (!data || !data.results) {
+    throw new Error("No data found")
+  }
+  return data.results.map((pokemon: any) => ({
+    name: pokemon.name,
+  }));
+}
+
+async function main() {
+  const pokemons = await getPokemon();
+  console.log(pokemons);
+}
+
+main();
